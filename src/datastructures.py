1eb924218c116469f070be579fbbf445e836bb99
natigalsan <natividadgalsan@gmail.com>
2022-07-19 10:04:31 +0000

    append

--- src/datastructures.py
+++ src/datastructures.py
@@ -13,15 +13,38 @@ class FamilyStructure:
         self.last_name = last_name
 
         # example list of members
-        self._members = []
+        self._members = [         
+         {
+                "id": self._generateId(),
+                "first_name": "John",
+                "last_name": "Jackson",
+                "age": "33 Years old",
+                "Lucky Numbers": [7, 13, 22]
+            },
+            {
+                "id": self._generateId(),
+                "first_name": "Jane",
+                "last_name":  "Jackson",
+                "age": "35 Years old",
+                "Lucky Numbers": [10, 14, 3]
+            },
+            {
+                "id": self._generateId(),
+                "first_name": "Jimmy",
+                "last_name":  "Jackson",
+                "age": "5 Years old",
+                "Lucky Numbers": [1]
+            }
+        ]
 
     # read-only: Use this method to generate random members ID's when adding members into the list
     def _generateId(self):
         return randint(0, 99999999)
 
     def add_member(self, member):
-        # fill this method and update the return
-        pass
+        # fill this method and update the return --> para agregar en Python --> <lista>.append(<elemento>)
+        self._members.append(member)
+        return None
 
     def delete_member(self, id):
         # fill this method and update the return
